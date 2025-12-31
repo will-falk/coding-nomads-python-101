5e5cbf72075a7a63c9cf864690ea28cdf5d4d509
will-sauer <will@heueso.com>
2025-12-31 13:24:11 -0500

    basic method structure and constants

---
+++ lab_04_build_something/guess.py
@@ -0,0 +1,34 @@
+#just going to roll my own guessing game
+#user has 5 tries to guess a number between 1 and 100
+#game gives feedback of whether each guess is above or below the number
+#after 5th fail, number is revealed
+#user can continue to press end to stop playing
+
+
+
+LOWEST_NUMBER = (1)
+HIGHEST_NUMBER = (100)
+MAX_GUESSES = (5)
+
+def ask_for_guess():
+    #prompt user for guess input
+    pass
+
+def evaluate_guess(guess, target_number):
+    #evaluate_guess checks if guess is correct, too high, or too low
+    pass
+
+def play():
+    #play game control flow initializes and cycles through guessing process, ends or resets at conclusion
+    pass
+
+def reset_game():
+    #reset game variables to start a new game
+    pass
+
+def end_game():
+    pass
+
+if __name__ == "__main__":
+    # ...call play
+    play()
