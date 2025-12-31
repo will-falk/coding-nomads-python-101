2567d79c3b7798e61de0ac6079687497e6b9b021
will-sauer <will@heueso.com>
2025-12-31 14:17:03 -0500

    pick_target_number

--- lab_04_build_something/guess.py
+++ lab_04_build_something/guess.py
@@ -1,25 +1,24 @@
+import random
+
 #just going to roll my own guessing game
 #user has 5 tries to guess a number between 1 and 100
 #game gives feedback of whether each guess is above or below the number
 #after 5th fail, number is revealed
 #user can continue to press end to stop playing
 
-
-
 LOWEST_NUMBER = (1)
 HIGHEST_NUMBER = (100)
 MAX_GUESSES = (5)
+
+def pick_target_number(low=LOWEST_NUMBER, high=HIGHEST_NUMBER):
+    return random.randint(low, high)
 
 def ask_for_guess():
     #prompt user for guess input
     pass
 
-def evaluate_guess(guess, target_number):
+def evaluate_guess(guess, target):
     #evaluate_guess checks if guess is correct, too high, or too low
-    pass
-
-def play():
-    #play game control flow initializes and cycles through guessing process, ends or resets at conclusion
     pass
 
 def reset_game():
@@ -27,6 +26,12 @@ def reset_game():
     pass
 
 def end_game():
+    pass
+
+def play():
+    #initialize
+    #cycle through guesses
+    #end or reset game
     pass
 
 if __name__ == "__main__":
