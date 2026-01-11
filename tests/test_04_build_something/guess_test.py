d213113568be35fde79d8cacbb28aaca6c8e08eb
will-sauer <will@heueso.com>
2026-01-11 13:54:58 -0500

    test validation only, ask_for_guess tested via integration

--- tests/test_04_build_something/guess_test.py
+++ tests/test_04_build_something/guess_test.py
@@ -3,3 +3,12 @@ import lab_04_build_something.guess as guess
 def test_pick_target_number():
     number = guess.pick_target_number()
     assert guess.LOWEST_NUMBER <= number <= guess.HIGHEST_NUMBER
+
+def test_validate_guess_valid():
+    assert guess.validate_guess('50') == 50
+    assert guess.validate_guess('dog') == None
+    assert guess.validate_guess('0') == None
+    assert guess.validate_guess('101') == None
+    assert guess.validate_guess('q') == 'q'
+    
+
