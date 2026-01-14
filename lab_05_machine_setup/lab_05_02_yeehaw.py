52dc45173ff7ac06640f1cdd791dd52642d94c2b
will-sauer <will@heueso.com>
2026-01-14 11:58:55 -0500

    remove non useful lab

--- lab_05_machine_setup/lab_05_02_yeehaw.py
+++
@@ -1,5 +0,0 @@
-# In this script, write code that displays the follow message:
-#
-# 	I'm a programmer now.
-# 	Yeehaw!
-# 	Coding here I come!
--- lab_05_machine_setup/lab_05_04_seconds_year.py
+++
@@ -1,2 +0,0 @@
-# Use the interpreter to calculate how many seconds are in a year.
-# Then write the code in this script file below this line.
